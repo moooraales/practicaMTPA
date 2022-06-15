894d70f6cfd2febf866af9e24a22169aab828c1a
Sergio Morales <sergio.moragal.va@gmail.com>
2022-06-15 16:22:44 +0200

    Prueba

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author sergi
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+}
