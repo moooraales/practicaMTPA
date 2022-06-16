cbd89422848a34543be9a040fc52b9df40043002
Ángel Puebla <apuebla11449@alumnos.uemc.es>
2022-06-16 09:39:37 +0200

    Sockets

--- Proyecto/src/main/java/Servidor/Cliente.java
+++ Proyecto/src/main/java/Servidor/Cliente.java
@@ -1,5 +1,22 @@
 package Servidor;
 
+
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Cliente {
+
+
+
+    public Cliente(){
+    }
+    
+    public Cliente(Socket socketCliente){
+        this.cliente = socketCliente;
+        this.usuario = null;
+        this.start();
+    }
     
 }
