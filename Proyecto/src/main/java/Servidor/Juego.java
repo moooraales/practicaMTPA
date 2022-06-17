c50f283fca4464d04541f3e3ed1a08ec199f8abe
Nacho <irodriguez20183@alumnos.uemc.es>
2022-06-17 10:53:33 +0200

    Implementacion clase Juego

---
+++ Proyecto/src/main/java/Servidor/Juego.java
@@ -0,0 +1,121 @@
+package Servidor;
+
+public class Juego {
+    private Usuario local;
+    private Usuario visitante;
+    private opcionJuego opcionLocal;
+    private opcionJuego opcionVisitante;
+    private int tiempoJugada;   // En milisegundos
+    private Usuario usuarioInfectado;
+    private int contadorVirus;
+
+    // Constructores
+    
+    public Juego(Usuario local, Usuario visitante) {    
+        this(local,visitante,null,null);
+    }
+    
+    public Juego(Usuario local, Usuario visitante, opcionJuego opcionLocal, opcionJuego opcionVisitante) {    
+        this.local = local;
+        this.visitante = visitante;
+        this.opcionLocal = opcionLocal;
+        this.opcionVisitante = opcionVisitante;
+        this.usuarioInfectado = null;
+        this.tiempoJugada = 10000;
+    }
+
+    // Set & Get
+
+    public void setContadorVirus(int contadorVirus){
+        this.contadorVirus = contadorVirus;
+    }
+    public void setUsuarioInfectado(Usuario usuarioInfectado) {    
+        this.usuarioInfectado = usuarioInfectado;
+    }
+    
+    public Usuario getUsuarioInfectado() {
+        return usuarioInfectado;
+    }
+
+    public void setOpcionLocal(opcionJuego opcionLocal) {
+        this.opcionLocal = opcionLocal;
+    }
+
+    public void setOpcionVisitante(opcionJuego opcionVisitante) {
+        this.opcionVisitante = opcionVisitante;
+    }
+    
+    public opcionJuego getOpcionLocal() {
+        return opcionLocal;
+    }
+
+    public opcionJuego getOpcionVisitante() {
+        return opcionVisitante;
+    }
+    
+    public int getTiempoJugada() {
+        return tiempoJugada;
+    }
+
+    //Metodos
+
+    protected Usuario obtenerGanador(){
+        Usuario ganador;
+        int resultado;
+        if(usuarioInfectado!=null && contadorVirus>0){
+            if(usuarioInfectado.equals(local)){
+                ganador = visitante;
+            }else{
+                ganador = local;
+            }
+            contadorVirus--;
+        }else{
+            if(opcionLocal==null){
+                ganador = visitante;
+            }else if(opcionVisitante == null){
+                ganador = local;
+            }else{
+                resultado = opcionLocal.contra(opcionVisitante);
+                switch (resultado) {
+                    case 1:
+                        ganador = local;
+                        break;
+                    case -1:
+                        ganador = visitante;
+                        break;
+                    default:
+                        //Empate
+                        ganador = null;
+                        break;
+                }
+            }
+        }
+        return ganador;
+    }
+
+    public Usuario obtenerResultado(){
+        boolean localActivo = true;
+        boolean visitanteActivo = true;
+        Usuario ganador;
+        
+        // Comprueba estado jugadores
+        if(Cliente.buscarClienteLogueado(local) == null){
+            localActivo = false;
+        }
+        if(Cliente.buscarClienteLogueado(visitante) == null){
+            visitanteActivo = false;
+        }
+        
+        // Ganador en funcion de inactividad
+        if(!localActivo && !visitanteActivo){
+            ganador = null;
+        }else if(!localActivo){
+            ganador = visitante;
+        }else if(!visitanteActivo){
+            ganador = local;
+        }else{  // Ganador por jugada
+            ganador = obtenerGanador();
+        }
+        return ganador;
+    }
+}
