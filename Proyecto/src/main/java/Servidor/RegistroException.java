ecb0cfc98dc47abcb018fbff8c7229f99de0e383
Ángel Puebla <apuebla11449@alumnos.uemc.es>
2022-07-06 12:55:46 +0200

    Excepcion del registro

---
+++ Proyecto/src/main/java/Servidor/RegistroException.java
@@ -0,0 +1,10 @@
+package Servidor;
+
+public class RegistroException extends Exception{
+
+    @Override
+    public String toString(){
+        return "Error en el registro de usuario";
+    }
+    
+}
