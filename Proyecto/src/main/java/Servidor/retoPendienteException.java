3a10ce5a35c3f61d08b2291a7479506828a0fede
Ángel Puebla <apuebla11449@alumnos.uemc.es>
2022-07-07 09:33:16 +0200

    Excepcion Reto Pendiente

---
+++ Proyecto/src/main/java/Servidor/retoPendienteException.java
@@ -0,0 +1,11 @@
+package Servidor;
+
+public class retoPendienteException extends Exception{
+
+    @Override
+    public String toString(){
+
+        return "Aun tienes un reto pendiente";
+    }
+    
+}
